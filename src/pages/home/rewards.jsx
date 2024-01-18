1bcdbe82f2e6dee3ca922d9b6f066ff742c0b251
Ayush Jain <ayushjain2205@gmail.com>
2024-01-18 18:15:23 +0530

    points page

--- src/pages/home/rewards.jsx
+++ src/pages/home/rewards.jsx
@@ -6,7 +6,15 @@ const rewards = () => {
   return (
     <Layout>
       <Decorator />
-      rewards
+      <div className="flex flex-col items-center">
+        <p className="text-[16px] font-[500] mt-[38px] mb-[16px] w-full">
+          WIN REWARDS
+        </p>
+        <div className="w-[271px] h-[277px] mb-[50px] rounded-[10px] bg-[#F4F7F6]"></div>
+        <button className="h-[45px] w-[213px] bg-[#000] rounded-[10px] text-white">
+          claim now
+        </button>
+      </div>
     </Layout>
   );
 };
